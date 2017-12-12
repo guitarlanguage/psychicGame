9088cfb1026cbfc4d3edc5724ac9f8baa4c3d2d1
Tom LaChance <guitarlanguageseries@gmail.com>
2017-12-12 16:32:20 -0600

    added background, image, tied all lose ends for games functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,10 @@
-<script type="text/javascript">
   // Creates an array that lists out all of the options that the  computer could use.
   var computerChoices = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j", "k", "l", "m",
   "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   // Creating global variables to hold the number of wins, losses. They start at 0. also a downward counter for guesses left
       var wins = 0;
       var losses = 0;
-      var guessesLeft = 9;
+      var guessesLeft = 15;
       var guesses = 9;
       var guessedLetters = [];
       var lettersToGuess = null;
@@ -13,104 +12,102 @@
 
   // Randomly chooses a choice from the computerChoice array. This is the Computer's guess.
   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
   // --------------------need to allow user 9 guesses------------------------------------
-//   function guess {
-//     for(var i = 0; i < 9; i++){
-//     console.log([i]);
-//   }
-// }
-// guess();
-  var updateChancesLeft = function(){
+  var updateGuessesLeft = function(){
     document.querySelector("#guessesLeft").innerText = "Guesses Left: " + guessesLeft;
   };
 
-
-    var updateLettersToGuess = function() {
+  var updateLetterToGuess = function() {
         this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-    };
+  };
 
   var updateGuessesSoFar = function() {
     //display the letters that the user has tried
     document.querySelector("#sofar").innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
   };
 
-
-
   var reset = function(){
 
-    // wins = 0;
-    // losses = 0;
-    guessesLeft = 9;
+    guessesLeft = 15;
     totalGuesses = 9;
     guessedLetters = [];
 
-    updateChancesLeft();
+    updateLetterToGuess();
+    updateGuessesLeft();
     updateGuessesSoFar();
-    updateLettersToGuess();
   }
 
-  updateLettersToGuess();
-  updateChancesLeft();
+  updateLetterToGuess();
+  updateGuessesLeft();
   //----------------------onkeyup section----------------------------------
   // This function is run whenever the user presses a key.
   document.onkeyup = function(event) {
-
     guessesLeft--;
     // Captures the key press, converts it to lowercase, and saves it to a variable.
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    var usersGuessHolder = function() {
-
-    for(var i = 0; i < 9; i++) {
     guessedLetters.push(userGuess);
-    console.log(userGuess);
-    }
-  }
-    // Determines which key was pressed.
+    updateGuessesLeft();
+    updateGuessesSoFar();
 
-
-    //   var multiGuess = function l() {
-    //    for(var i = 0; i < 9; i++) {
-    //    userGuessArray.push(userGuess);
-    //   }]
-    // }
-    //    return userGuess;
-
-
-
-    // Reworked our code from last step to use "else if" instead of lots of if statements.
-
-
-    if(userGuess === computerGuess) {
+    if(guessesLeft > 0){
+      if(userGuess === letterToGuess){
         wins++;
-        // guessesLeft--;
-      } else if (userGuess !== computerGuess) {
-        losses++;
-
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
+        alert("Psychic powers to the 10th power!");
+        reset();
       }
-
-
-    if(guessesLeft === 0) {
-      document.write("Game Over");
-
+    }else if(guessesLeft === 0) {
+        losses++;
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        alert("Game Over");
+        reset();
     }
+  };
+
+
+
+
+    // if(userGuess === computerGuess) {
+    //     wins++;
+    //     // guessesLeft--;
+    //   } else if (userGuess !== computerGuess) {
+    //     losses++;
+    //
+    //   }
+    //
+    //
+    // if(guessesLeft === 0) {
+    //   document.write("Game Over");
+    //
+    // }
 
 
 
 
       // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-      var html =
-        "<h1>Genie Psychic game" + "</h1>" +
-        "<p>You chose: " + userGuess + "</p>" +
-        "<p>The computer chose: " + computerGuess + "</p>" +
-        "<p>wins: " + wins + "</p>" +
-        "<p>losses: " + losses + "</p>" +
-        "<p>Guesses Left: " + guessesLeft + "</p>" +
-        "<p>Your guesses so far: " + userGuess + "</p>";
+      // var html =
+      //   "<h1>Genie Psychic game" + "</h1>" +
+      //   "<p>You chose: " + userGuess + "</p>" +
+      //   "<p>The computer chose: " + computerGuess + "</p>" +
+      //   "<p>wins: " + wins + "</p>" +
+      //   "<p>losses: " + losses + "</p>" +
+      //   "<p>Guesses Left: " + guessesLeft + "</p>" +
+      //   "<p>Your guesses so far: " + userGuess + "</p>";
 
 
 
       // Set the inner HTML contents of the #game div to our html string
-      document.querySelector("#game").innerHTML = html;
+      // document.querySelector("#game").innerHTML = html;
 
-  };
+
+
+  // var usersGuessHolder = function() {
+  // //
+  // //   for(var i = 0; i < 9; i++) {
+  // //   guessedLetters.push(userGuess);
+  // //   console.log(userGuess);
+  // //   }
+  // // }
